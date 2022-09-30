c72671fcc434c448e4271672b97b5addd4b5ac86
王天赐 <15565946702@163.com>
2022-09-30 11:06:34 +0800

    Initial commit

---
+++ sql/基本的SELECT.sql
@@ -0,0 +1,28 @@
+
+# SELECT
+SELECT 1;
+
+# SELECT 全部字段
+
+SELECT * FROM departments;
+
+# SELECT 字段
+USE atguigudb;
+SELECT department_id, department_name FROM departments;
+
+# 列的别名
+SELECT department_id AS "id", department_name AS "name" FROM departments;
+
+# 去除重复行
+
+SELECT DISTINCT department_id,salary  FROM employees;
+
+# 空值运算
+
+SELECT employee_id,salary,commission_pct, 12 * salary * (1+commission_pct) AS "annual_sal" from employees;
+
+# SELECT * FROM ORDER , 这是错误的, 因为 ORDER 和 MySQL 关键字相同了
+SELECT * FROM `order`
+
+# 常数
+
